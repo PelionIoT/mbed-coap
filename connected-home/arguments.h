168a1342135aa19e19f017ea341ab2a03d71575a
zach <zach@sensinode.com>
2012-06-28 08:40:10 +0000

    Initial check-in for connected-home example.
    git-svn-id: svn+ssh://192.168.0.9/home/common-files/subversion/devel/CoAP/nsdl-c-simple@13992 b4939478-c410-0410-9151-a6aaa495c60e

---
+++ connected-home/arguments.h
@@ -0,0 +1,36 @@
+/**
+ * \file 	arguments.h
+ *
+ * \brief	Arguments for command line parameters
+ *
+ * \author 	Zach Shelby <zach@sensinode.com>
+ *
+ */
+
+#ifndef _ARGUMENTS_
+#define _ARGUMENTS_
+
+#include "inttypes.h"
+
+#define FALSE 0
+#define TRUE 1
+
+#ifdef MACOSX 
+// ----------------------------------------
+// Mac OS X
+#ifdef __DARWIN_UNIX03
+typedef unsigned short port_t;
+#else
+#include <mach/port.h>
+#endif // __DARWIN_UNIX03
+typedef unsigned long ipaddr_t;
+#endif
+
+/* Argument variables */
+uint8_t arg_gui;
+uint16_t arg_port, arg_sport, arg_dport, arg_dtlsport;	//0-65535
+char arg_dst[64];
+
+#endif /* _ARGUMENTS_ */
+
+
