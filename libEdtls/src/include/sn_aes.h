6cf30d3e7382181ffad022e8ebbd317a5bc5a861
Seppo Takalo <seppo.takalo@arm.com>
2014-06-11 05:04:38 -0500

    Remove old junk, libEdtls no here anymore

--- libEdtls/src/include/sn_aes.h
+++
@@ -1,7 +0,0 @@
-#ifndef SN_AES
-#define SN_AES
-
-void aes_encrypt(unsigned char *plaintext_ptr, unsigned char *aes_key_ptr);
-void aes_decrypt(unsigned char *ciphertext_ptr, unsigned char *aes_key_ptr);
-
-#endif
