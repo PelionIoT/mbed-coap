04a318ec406c000bc9dfb8a74f9610d547c9ae7b
Juha Heiskanen <juha.heiskanen@arm.com>
2014-06-30 16:38:36 +0300

    Updated aclara project copy

--- libCoap/arm_iar/aclara_lib_copy.bat
+++ libCoap/arm_iar/aclara_lib_copy.bat
@@ -1,9 +1,9 @@
 
 mkdir %1\..\..\..\Aclara_Node_Project\ARM_Libraries\
-mkdir %1\..\..\..\Aclara_Node_Project\ARM_Libraries\Include
+mkdir %1\..\..\..\Aclara_Node_Project\ARM_Libraries\Include\nsdl
 mkdir %1\..\..\..\Aclara_Node_Project\ARM_Libraries\Binary
 copy %1\*.a %1\..\..\..\Aclara_Node_Project\ARM_Libraries\Binary\
 
-copy %1\..\src\include\sn_coap_header.h %1\..\..\..\Aclara_Node_Project\ARM_Libraries\Include\
-copy %1\..\src\include\sn_coap_protocol.h %1\..\..\..\Aclara_Node_Project\ARM_Libraries\Include\
-copy %1\..\src\include\sn_nsdl.h %1\..\..\..\Aclara_Node_Project\ARM_Libraries\Include\
+copy %1\..\src\include\sn_coap_header.h %1\..\..\..\Aclara_Node_Project\ARM_Libraries\Include\nsdl
+copy %1\..\src\include\sn_coap_protocol.h %1\..\..\..\Aclara_Node_Project\ARM_Libraries\Include\nsdl
+copy %1\..\src\include\sn_nsdl.h %1\..\..\..\Aclara_Node_Project\ARM_Libraries\Include\nsdl
--- libNsdl/arm_iar/aclara_lib_copy.bat
+++ libNsdl/arm_iar/aclara_lib_copy.bat
@@ -1,7 +1,7 @@
 
 mkdir %1\..\..\..\Aclara_Node_Project\ARM_Libraries\
-mkdir %1\..\..\..\Aclara_Node_Project\ARM_Libraries\Include
+mkdir %1\..\..\..\Aclara_Node_Project\ARM_Libraries\Include\nsdl
 mkdir %1\..\..\..\Aclara_Node_Project\ARM_Libraries\Binary
 copy %1\*.a %1\..\..\..\Aclara_Node_Project\ARM_Libraries\Binary\
 
-copy %1\..\src\Include\sn_nsdl_lib.h %1\..\..\..\Aclara_Node_Project\ARM_Libraries\Include\
+copy %1\..\src\Include\sn_nsdl_lib.h %1\..\..\..\Aclara_Node_Project\ARM_Libraries\Include\nsdl
