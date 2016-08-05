95a4f1753690598b582e2132893534393c2c7b62
Antti Yli-Tokola <antti.yli-tokola@arm.com>
2016-08-05 09:03:28 +0300

    Change build time flags

--- source/libCoap/src/include/sn_coap_protocol_internal.h
+++ source/libCoap/src/include/sn_coap_protocol_internal.h
@@ -90,7 +90,7 @@ extern "C" {
 #endif
 
 #ifndef SN_COAP_MAX_BLOCKWISE_PAYLOAD_SIZE
-#define SN_COAP_MAX_BLOCKWISE_PAYLOAD_SIZE          128  /**< Must be 2^x and x is at least 4. Suitable values: 0, 16, 32, 64, 128, 256, 512 and 1024 */
+#define SN_COAP_MAX_BLOCKWISE_PAYLOAD_SIZE          0  /**< Must be 2^x and x is at least 4. Suitable values: 0, 16, 32, 64, 128, 256, 512 and 1024 */
 #endif
 
 
@@ -99,7 +99,7 @@ extern "C" {
 #endif
 
 #ifndef SN_COAP_MAX_BLOCK1_MESSAGE_SIZE
-#define SN_COAP_MAX_BLOCK1_MESSAGE_SIZE 74000
+#define SN_COAP_MAX_BLOCK1_MESSAGE_SIZE UINT32_MAX
 #endif
 
 /* * * * * * * * * * * * * * */
