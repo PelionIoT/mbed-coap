5a1de97739c343cd74033358427a941daa671a50
Tero Jääskö <tero.jaasko@arm.com>
2016-08-26 19:11:27 +0300

    Clone the Makefile to *.standalone to allow compilation via new system
    The experimental compilation system processes Makefile.standalone automatically,
    so clone the existing makefile for it. This should be only temporary stop gap,
    as there really is no point to have same content duplicated.

---
+++ include_dirs.mk
@@ -0,0 +1,12 @@
+INCLUDE_DIRS := \
+	../ \
+	. \
+	../../nsdl-c \
+	../../mbed-trace \
+        ../lwm2m-client \
+        ../lwm2m-client-linux/source \
+        source \
+        ../mbed-client \
+        ../mbed-client-c \
+        ../mbed-trace \
+	../nanostack-libservice/mbed-client-libservice \
