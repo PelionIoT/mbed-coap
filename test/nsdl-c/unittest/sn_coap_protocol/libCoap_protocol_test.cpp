248fc713a4303cb1f724d10f4aa6d68933bb5aed
Yogesh Pande <Yogesh.Pande@arm.com>
2016-09-24 11:32:01 +0300

    Fix for failing unit tests

--- test/nsdl-c/unittest/sn_coap_protocol/libCoap_protocol_test.cpp
+++ test/nsdl-c/unittest/sn_coap_protocol/libCoap_protocol_test.cpp
@@ -87,9 +87,13 @@ TEST(libCoap_protocol, sn_coap_protocol_destroy)
     msg_ptr->send_msg_ptr->uri_path_len = 2;
 
     ns_list_add_to_end(&handle->linked_list_resent_msgs, msg_ptr);
+#if SN_COAP_DUPLICATION_MAX_MSGS_COUNT
     ns_list_init(&handle->linked_list_duplication_msgs);
+#endif
+#if SN_COAP_MAX_BLOCKWISE_PAYLOAD_SIZE
     ns_list_init(&handle->linked_list_blockwise_sent_msgs);
     ns_list_init(&handle->linked_list_blockwise_received_payloads);
+#endif
     CHECK( 0 == sn_coap_protocol_destroy(handle));
 }
 
