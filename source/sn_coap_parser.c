a0144c32315fa3de9840c177d4dac98ae0506dcf
Tero Jääskö <tero.jaasko@arm.com>
2019-03-28 18:56:07 +0200

    parser, protocol: add util for message+options alloc -pair and use it
    A common pattern of alloc message+options pair can be replaced with
    a helper function, which reduces code size and also simplifies logic.

--- source/sn_coap_parser.c
+++ source/sn_coap_parser.c
@@ -64,6 +64,29 @@ sn_coap_hdr_s *sn_coap_parser_init_message(sn_coap_hdr_s *coap_msg_ptr)
     return coap_msg_ptr;
 }
 
+sn_coap_hdr_s *sn_coap_parser_alloc_message_with_options(struct coap_s *handle)
+{
+    // check the handle just as in any other place
+    if (handle == NULL) {
+        return NULL;
+    }
+
+    sn_coap_hdr_s *coap_msg_ptr = sn_coap_parser_alloc_message(handle);
+
+    sn_coap_options_list_s *options_list_ptr = sn_coap_parser_alloc_options(handle, coap_msg_ptr);
+
+    if ((coap_msg_ptr == NULL) || (options_list_ptr == NULL)) {
+
+        // oops, out of memory free if got already any
+        handle->sn_coap_protocol_free(coap_msg_ptr);
+        handle->sn_coap_protocol_free(options_list_ptr);
+
+        coap_msg_ptr = NULL;
+    }
+
+    return coap_msg_ptr;
+}
+
 sn_coap_hdr_s *sn_coap_parser_alloc_message(struct coap_s *handle)
 {
     sn_coap_hdr_s *returned_coap_msg_ptr;
@@ -109,7 +132,6 @@ sn_coap_options_list_s *sn_coap_parser_alloc_options(struct coap_s *handle, sn_c
     options_list_ptr->accept = COAP_CT_NONE;
     options_list_ptr->block2 = COAP_OPTION_BLOCK_NONE;
     options_list_ptr->block1 = COAP_OPTION_BLOCK_NONE;
-
 
     return options_list_ptr;
 }
