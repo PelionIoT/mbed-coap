49a286ab16059ec016465a69f71bf8711e8c05fe
Tero Jääskö <tero.jaasko@arm.com>
2019-03-29 13:28:04 +0200

    parser, microscopic changes, few dozens of bytes

--- source/sn_coap_parser.c
+++ source/sn_coap_parser.c
@@ -180,27 +180,35 @@ void sn_coap_parser_release_allocated_coap_msg_mem(struct coap_s *handle, sn_coa
     }
 
     if (freed_coap_msg_ptr != NULL) {
-        handle->sn_coap_protocol_free(freed_coap_msg_ptr->uri_path_ptr);
-        handle->sn_coap_protocol_free(freed_coap_msg_ptr->token_ptr);
 
-        if (freed_coap_msg_ptr->options_list_ptr != NULL) {
+        // As there are multiple sequential calls to the protocol_free, caching pointer to it
+        // saves one instruction per call.
+        void (*local_free)(void *) = handle->sn_coap_protocol_free;
 
-            handle->sn_coap_protocol_free(freed_coap_msg_ptr->options_list_ptr->proxy_uri_ptr);
+        local_free(freed_coap_msg_ptr->uri_path_ptr);
+        local_free(freed_coap_msg_ptr->token_ptr);
 
-            handle->sn_coap_protocol_free(freed_coap_msg_ptr->options_list_ptr->etag_ptr);
+        // same here, caching the struct start saves a bit.
+        sn_coap_options_list_s *options_list_ptr = freed_coap_msg_ptr->options_list_ptr;
 
-            handle->sn_coap_protocol_free(freed_coap_msg_ptr->options_list_ptr->uri_host_ptr);
+        if (options_list_ptr != NULL) {
 
-            handle->sn_coap_protocol_free(freed_coap_msg_ptr->options_list_ptr->location_path_ptr);
+            local_free(options_list_ptr->proxy_uri_ptr);
 
-            handle->sn_coap_protocol_free(freed_coap_msg_ptr->options_list_ptr->location_query_ptr);
+            local_free(options_list_ptr->etag_ptr);
 
-            handle->sn_coap_protocol_free(freed_coap_msg_ptr->options_list_ptr->uri_query_ptr);
+            local_free(options_list_ptr->uri_host_ptr);
 
-            handle->sn_coap_protocol_free(freed_coap_msg_ptr->options_list_ptr);
+            local_free(options_list_ptr->location_path_ptr);
+
+            local_free(options_list_ptr->location_query_ptr);
+
+            local_free(options_list_ptr->uri_query_ptr);
+
+            local_free(options_list_ptr);
         }
 
-        handle->sn_coap_protocol_free(freed_coap_msg_ptr);
+        local_free(freed_coap_msg_ptr);
     }
 }
 
